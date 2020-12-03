852b22dfc81d96c081a0ca8ca0338489d0d3750a
Nick Vogt <nick.vogt@dynatrace.com>
2020-12-04 06:37:57 +0800

    Taboggan Trajectory: Part 2

--- libs/tobaggan_trajectory/tests/test_solutions.py
+++ libs/tobaggan_trajectory/tests/test_solutions.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from functools import reduce
 import unittest
 from libs.tobaggan_trajectory import TOBAGGAN_TRAJECTORY_DEFAULT_INPUT, main
 
@@ -7,6 +8,18 @@ class TestSolution(unittest.TestCase):
     def test_part_1(self):
         expected = 216
         actual = main(x=3, y=1, input_file=TOBAGGAN_TRAJECTORY_DEFAULT_INPUT)
+        assert actual == expected
+
+    def test_part_2(self):
+        expected = 6708199680
+        results = [
+            main(x=1, y=1, input_file=TOBAGGAN_TRAJECTORY_DEFAULT_INPUT),
+            main(x=3, y=1, input_file=TOBAGGAN_TRAJECTORY_DEFAULT_INPUT),
+            main(x=5, y=1, input_file=TOBAGGAN_TRAJECTORY_DEFAULT_INPUT),
+            main(x=7, y=1, input_file=TOBAGGAN_TRAJECTORY_DEFAULT_INPUT),
+            main(x=1, y=2, input_file=TOBAGGAN_TRAJECTORY_DEFAULT_INPUT),
+        ]
+        actual = reduce((lambda x, y: x * y), results)
         assert actual == expected
 
 
